8f0f1d7f84d491c54f7e5f333f34dc5daf139a01
Mao Wtm <m@maowtm.org>
2020-07-24 23:18:39 +0100

    Fix errors

--- src/main/java/io/github/Laplace/HideAndSeek/HideAndSeek.java
+++ src/main/java/io/github/Laplace/HideAndSeek/HideAndSeek.java
@@ -1,24 +1,26 @@
 package io.github.Laplace.HideAndSeek;
 
-import java.util.Arrays;
-import java.util.Collections;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Effect;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import java.util.*;
+import org.bukkit.*;
+import org.bukkit.event.*;
+import org.bukkit.command.*;
+import org.bukkit.entity.*;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
 
-public final class HideAndSeek extends JavaPlugin{
+public final class HideAndSeek extends JavaPlugin implements Listener {
 	@Override
     public void onEnable() {
+			PluginManager pm = this.getServer().getPluginManager();
+			pm.registerEvents(this, this);
     }
-    
+
     @Override
     public void onDisable() {
-    }
+		}
+
+		@EventHandler
     public void onPlayerDeath(PlayerDeathEvent event)
     {
     	event.setDeathMessage("What...!");//So no one knows who killed whom
@@ -33,7 +35,8 @@ public final class HideAndSeek extends JavaPlugin{
         //Preys all -2 hearts
         //Minimum 5 hearts and maximum 15 hearts
         //Alternative: if the dead Prey was killed by another Prey, do the above actions
-    }
+		}
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     	if (cmd.getName().equalsIgnoreCase("addPlayers")) {
@@ -45,19 +48,17 @@ public final class HideAndSeek extends JavaPlugin{
     		return true;
     	}
     	if (cmd.getName().equalsIgnoreCase("setTeam")) {
-    		
     		return true;
     	}
     	if (cmd.getName().equalsIgnoreCase("listTeams")) {
-    		
     		return true;
     	}
     	if (cmd.getName().equalsIgnoreCase("endGame")) {
-    		
     		return true;
     	}
-    	return false; 
-    }
+    	return false;
+		}
+
     public void TeamPlayers() {
     	Player[] players = (Player[])Bukkit.getOnlinePlayers().toArray();
     	Collections.shuffle(Arrays.asList(players));
@@ -72,7 +73,8 @@ public final class HideAndSeek extends JavaPlugin{
     		}
     		Team.Prey.AddPlayer(p);
     	}
-    }
+		}
+
     public void EquipPlayers() {
     	//TODO:
 		//Give players some items according to their roles
